3e53eda2e34e154c0d69e3fe144293ed40b30d09
Deniz Sokullu <denizaydinsokullu@gmail.com>
2018-11-15 13:59:02 -0500

    Feature/heroku deployment (#10)
    * added prod aliases
    
    * added heroku deployment specifics
    
    * fixes typo
    
    * removes unnecessary build
    
    * fixes routing
    
    * fixes directory
    
    * fixes directory
    
    * fixes directory
    
    * fixes directory
    
    * fixes directory
    
    * fixes directory
    
    * fixes directory
    
    * adds logrocket

---
+++ scripts/serve.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
+
+console.log('Launching Quo...');
+
+app.listen(port, () => {
+  console.log(`Quo is running on http://localhost:${port}`);
+});
