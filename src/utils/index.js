ac62a7da973d01d98344d5e3b30745c5a5f282e1
Deniz Sokullu <deniz.sokullu@modolabs.com>
2018-12-25 21:06:38 -0500

    removed helper deepFreeze

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,20 +2,3 @@
 const fExists = f => typeof f === 'function';
 export const fSafe = (f, ...args) =>  fExists(f) ? f(...args) : undefined;
 export const bool2s = (props, prop, suffix='style')=> props[prop] ? `${prop}-${suffix}` : '';
-
-export const deepFreeze = obj => {
-
-  // Retrieve the property names defined on object
-  var propNames = Object.getOwnPropertyNames(obj);
-
-  // Freeze properties before freezing self
-  
-  for (let name of propNames) {
-    let value = obj[name];
-
-    obj[name] = value && typeof value === "object" ? 
-      deepFreeze(value) : value;
-  }
-
-  return Object.freeze(obj);
-}
