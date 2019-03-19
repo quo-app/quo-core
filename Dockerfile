a191ccce3754f1478a02523513cd7422389bbd8e
Deniz Sokullu <deniz.sokullu@modolabs.com>
2019-03-19 16:58:56 -0400

    docker setup

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ RUN mkdir -p /quo-core
 WORKDIR /quo-core
 COPY package*.json /quo-core/
 RUN npm install
+RUN npm rebuild node-sass
 COPY . /quo-core
--- scripts/serve.js
+++ scripts/serve.js
@@ -3,7 +3,7 @@ const path = require('path');
 const compression = require('compression')
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(compression())
 app.use(express.static(path.join(__dirname, '../build')));
